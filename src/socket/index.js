7d8f75542cf7e91b0ef13b4db86ef32ec6cba4f3
chykea <78532156@qq.com>
2024-02-27 23:21:50 +0800

    代码优化

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,33 +2,32 @@ const socketIO = require('socket.io')
 function createSocket(server) {
     // socket跨域问题解决
     const io = socketIO(server, { cors: true })
-    let codeHistory = null // 用于保存最新
-    let codeSetting = null
-    let canvasHistory = null
+    let historyMap = new Map() // 用于保存历史
+    let history = {
+        codeHistory: null, // 用于保存最新
+        codeSetting: null,
+        canvasHistory: null,
+    }
+
     io.on('connection', (socket) => {
         // socket 即当前连接的用户
         socket.on('join', (data) => {
             socket.join(data.roomId)
             socket.broadcast.to(data.roomId).emit('join', `${data.user.user_name}进入房间`)
-            if (codeHistory) {
-                // 如果教师已经编辑
-                socket.emit('getCode', codeHistory)
-            }
-            if (codeSetting) {
-                // 当前用户连接且教师已经编辑
-                socket.emit('share', codeSetting)
-            }
-            if (canvasHistory) {
-                socket.emit('getPaint', canvasHistory)
-            }
+
+            // 如果教师已经编辑
+            socket.emit('initCode', historyMap.get(data.roomId)?.codeHistory || null)
+            // 当前用户连接且教师已经编辑
+            socket.emit('initSetting', historyMap.get(data.roomId)?.codeSetting || null)
+            socket.emit('initImage', historyMap.get(data.roomId)?.canvasHistory || null)
+
         })
         socket.on('leave', (roomId) => {
             // 离开房间倒不用广播
             socket.leave(roomId)
-            if (!(io.sockets.adapter.rooms.get(roomId).size)) {
+            if (!(io.sockets.adapter.rooms.get(roomId) && io.sockets.adapter.rooms.get(roomId).size)) {
                 // 没有用户,就获取不到房间,直接清空
-                codeHistory = null
-                codeSetting = null
+                historyMap.delete(roomId)
             }
         })
 
@@ -37,20 +36,23 @@ function createSocket(server) {
                 socket.broadcast.to(data.roomId).emit('getMsg', data)
             }
             else {
-                codeHistory = { roomId: data.roomId, user: data.user, code: data.code }
+                history.codeHistory = { roomId: data.roomId, user: data.user, code: data.code }
+                historyMap.set(data.roomId, history)
                 socket.broadcast.to(data.roomId).emit('getCode', data)
             }
             cb && cb({ code: "200", msg: "消息发送成功" })
         })
 
         socket.on('openShare', (data) => {
-            codeSetting = { roomId: data.roomId, user: data.user, readOnly: !data.isShare }
+            history.codeSetting = { roomId: data.roomId, user: data.user, readOnly: !data.isShare }
+            historyMap.set(data.roomId, history)
             // 开启共享即关闭只读
             socket.broadcast.to(data.roomId).emit('share', { user: data.user, readOnly: !data.isShare })
         })
 
         socket.on('sendPaint', (data) => {
-            // console.log(data);
+            history.canvasHistory = { roomId: data.roomId, user: data.user, imgSrc: data.data }
+            historyMap.set(data.roomId, history)
             socket.broadcast.to(data.roomId).emit('getPaint', data)
         })
     })
