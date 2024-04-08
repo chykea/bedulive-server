b7bee2ee78f2471bfec4222bab21dd434c392ef2
chykea <78532156@qq.com>
2024-04-08 22:32:31 +0800

    未使用ot解决版本

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,73 +1,88 @@
 const socketIO = require('socket.io')
 const { NodeVM } = require('vm2')
+const { Doc, applyUpdate } = require('yjs')
+
 function createSocket(server) {
     // socket跨域问题解决
     const io = socketIO(server, { cors: true })
-    let historyMap = new Map() // 用于保存对应房间的历史消息
-    let history = {
-        codeHistory: null, // 用于保存直播间最新消息
-        codeSetting: null,
-        canvasHistory: null,
-    }
-    // let toolUsers = []
-    let toolUsers = new Map()
-    let roomsOwners = new Map()
-    io.on('connection', (socket) => {
+    const historyMap = new Map() // 用于保存对应房间的历史消息
+
+    const liveConnect = io.of("/live");
+    liveConnect.on('connection', (socket) => {
         // 直播间
         // socket 即当前连接的用户
-        socket.on('join', (data) => {
-            socket.join(data.roomId)
-            socket.emit('currentUser', io.sockets.adapter.rooms.get(data.roomId).size)
-            socket.broadcast.to(data.roomId).emit('currentUser', io.sockets.adapter.rooms.get(data.roomId).size)
+        socket.on('joinLive', ({ roomId, user }) => {
+            socket.join(roomId)
+            socket.emit('currentUser', liveConnect.adapter.rooms.get(roomId).size)
+            socket.broadcast.to(roomId).emit('currentUser', liveConnect.adapter.rooms.get(roomId).size)
             // 如果教师已经编辑
-            socket.emit('initCode', historyMap.get(data.roomId)?.codeHistory || null)
+            socket.emit('initCode', historyMap.get(roomId)?.codeHistory || null)
             // 当前用户连接且教师已经编辑
-            socket.emit('initSetting', historyMap.get(data.roomId)?.codeSetting || null)
-            socket.emit('initImage', historyMap.get(data.roomId)?.canvasHistory || null)
-
+            socket.emit('initSetting', historyMap.get(roomId)?.codeSetting || null)
+            socket.emit('initImage', historyMap.get(roomId)?.canvasHistory || null)
         })
-        socket.on('leave', (roomId) => {
-
-            (io.sockets.adapter.rooms.get(roomId)) && socket.broadcast.to(roomId).emit('currentUser', io.sockets.adapter.rooms.get(roomId).size - 1)
-            socket.leave(roomId)
-            if (!(io.sockets.adapter.rooms.get(roomId) && io.sockets.adapter.rooms.get(roomId).size)) {
-                // 没有用户,就获取不到房间,直接清空
+        socket.on('leaveLive', ({ roomId, user }) => {
+            socket.leave(roomId);
+            (liveConnect.adapter.rooms.get(roomId)) && socket.broadcast.to(roomId).emit('currentUser', liveConnect.adapter.rooms.get(roomId).size)
+            // 最后一个用户离开后,清空
+            if (!(liveConnect.adapter.rooms.get(roomId))) {
                 historyMap.delete(roomId)
+                return
             }
         })
         socket.on('updateRoomInfo', (data) => {
             socket.broadcast.to(data.roomId).emit('updateRoom', data.roomInfo)
         })
-        socket.on('sendMsg', (data, cb) => {
-            if (data.type == 'chat') {
-                socket.broadcast.to(data.roomId).emit('getMsg', data)
-            }
-            else {
-                history.codeHistory = { roomId: data.roomId, user: data.user, code: data.code }
-                historyMap.set(data.roomId, history)
-                socket.broadcast.to(data.roomId).emit('getCode', data)
-            }
-            cb && cb({ code: "200", msg: "消息发送成功" })
+        socket.on('sendMsg', (data) => {
+            socket.broadcast.to(data.roomId).emit('getMsg', data)
         })
-        socket.on('openShare', (data) => {
-            history.codeSetting = { roomId: data.roomId, user: data.user, readOnly: !data.isShare }
-            historyMap.set(data.roomId, history)
+        socket.on('sendCode', (data) => {
+            let origin = historyMap.get(data.roomId) || {
+                codeHistory: null,
+                codeSetting: null,
+                canvasHistory: null,
+            }
+            const codeHistory = { ...data }
+            historyMap.set(data.roomId, { ...origin, codeHistory })
+            socket.broadcast.to(data.roomId).emit('getCode', data)
+        })
+        socket.on('openShare', ({ roomId, readOnly }) => {
+            const origin = historyMap.get(roomId) || {
+                codeHistory: null,
+                codeSetting: null,
+                canvasHistory: null,
+            }
+            const codeSetting = { readOnly }
+            historyMap.set(roomId, { ...origin, codeSetting })
             // 开启共享即关闭只读
-            socket.broadcast.to(data.roomId).emit('share', { user: data.user, readOnly: !data.isShare })
+            socket.broadcast.to(roomId).emit('share', codeSetting)
         })
         socket.on('sendPaint', (data) => {
-            history.canvasHistory = { roomId: data.roomId, user: data.user, imgSrc: data.data }
-            historyMap.set(data.roomId, history)
+            let origin = historyMap.get(data.roomId) || {
+                codeHistory: null,
+                codeSetting: null,
+                canvasHistory: null,
+            }
+            const canvasHistory = { ...data }
+            historyMap.set(data.roomId, { ...origin, canvasHistory })
             socket.broadcast.to(data.roomId).emit('getPaint', data)
         })
+    })
 
-
-        // 工具页代码共享
+    const toolUsers = new Map()
+    const roomsOwners = new Map()
+    const toolConnect = io.of("/tool");
+    // const docMap = new Map()
+    // 工具页代码共享
+    toolConnect.on('connection', (socket) => {
+        // 如果用户已经连接到socket服务,则不再执行连接操作
         // 创建房间
         socket.on('createRoom', (data) => {
-            if (historyMap.get(data.roomId)) {
+
+            if (toolConnect.adapter.rooms.get(data.roomId)) {
                 return
             }
+            const doc = new Doc();
             // 房主
             roomsOwners.set(data.roomId, socket.id)
             socket.join(data.roomId)
@@ -76,6 +91,8 @@ function createSocket(server) {
             toolUsers.set(data.roomId, users)
             // 存储代码/图片
             historyMap.set(data.roomId, { code: data.code || '', imgSrc: data.imgSrc || '' })
+            // 存储对应房间的文档
+            // docMap.set(data.roomId, doc)
         })
         socket.on('sendCode', (data) => {
             let origin = historyMap.get(data.roomId)
@@ -87,9 +104,8 @@ function createSocket(server) {
             historyMap.set(data.roomId, { ...origin, imgSrc: data.imgSrc })
             socket.broadcast.to(data.roomId).emit('receiveImg', data)
         })
-
         socket.on('joinRoom', (data) => {
-            if (!(io.sockets.adapter.rooms.get(data.roomId))) {
+            if (!(toolConnect.adapter.rooms.get(data.roomId))) {
                 socket.emit('roomNotExist')
                 return
             }
@@ -110,11 +126,12 @@ function createSocket(server) {
         socket.on('leaveRoom', (data) => {
             socket.leave(data.roomId)
             // 最后一个用户离开后,清空
-            if (!(io.sockets.adapter.rooms.get(data.roomId))) {
+            if (!(toolConnect.adapter.rooms.get(data.roomId))) {
                 // 清空当前房间的用户
                 toolUsers.delete(data.roomId)
                 // 清空当前房间的历史数据
                 historyMap.delete(data.roomId)
+                // docMap.delete(data.roomId)
                 return
             }
             // 房主离开
@@ -123,13 +140,13 @@ function createSocket(server) {
                 socket.broadcast.to(data.roomId).emit('roomOwnerLeave', { roomId: data.roomId, user: data.user })
                 toolUsers.delete(data.roomId)
                 historyMap.delete(data.roomId)
+                // docMap.delete(data.roomId)
                 return
             }
             // 用户列表删除该用户
             let curUserList = toolUsers.get(data.roomId)
             curUserList = curUserList.filter(item => item.uid !== data.user.uid)
             toolUsers.set(data.roomId, curUserList)
-
             // 用户已离开房间(需要提醒哪个用户离开)
             socket.broadcast.to(data.roomId).emit('userLeave', { userList: curUserList })
             const history = historyMap.get(data.roomId)
@@ -144,9 +161,22 @@ function createSocket(server) {
             })
         })
 
+        /* socket.on('update:yjs', async ({ roomId, update }) => {
+            const doc = docMap.get(roomId)
+            applyUpdate(doc, update)
+            socket.broadcast.to(roomId).emit('update', update)
+        })
+        socket.on('init:state', ({ roomId, code }) => {
+            let origin = historyMap.get(roomId) || {
+                code: '',
+                imgSrc: ''
+            }
+            historyMap.set(roomId, { ...origin, code })
+        }) */
 
-        socket.on('runCode', async data => {
-            const code = data.code;
+
+        socket.on('runCode', async ({ code }) => {
+            console.log(toolConnect.adapter.rooms);
             const vm = new NodeVM({
                 timeout: 2000, // 限制代码运行时间
                 console: 'redirect',
@@ -161,6 +191,7 @@ function createSocket(server) {
 
             try {
                 await vm.run(code);
+                console.log(res);
                 // 代码运行结果
                 socket.emit('runRes', res)
             } catch (error) {
